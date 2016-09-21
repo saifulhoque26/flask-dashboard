1fad4095776b9cb45cb9821157fe50d4bb5e7980
Tom Rafferty <thomasrafferty@gmail.com>
2016-09-21 22:31:00 +0000

    create AJAX looping...

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
- 
+
+idx_val = 0
+
 @app.before_request
 def before_request():
     if request.path != '/':
@@ -16,5 +18,19 @@ def echo():
     ret_data = {"value": request.args.get('echoValue')}
     return jsonify(ret_data)
  
+@app.route('/idx/', methods=['GET'])
+def idx():
+    global idx_val
+    idx_val += 1
+    ret_data = {"value": idx_val}
+    return jsonify(ret_data)
+ 
+@app.route('/reset/', methods=['GET'])
+def reset():
+    global idx_val
+    idx_val = 0
+    ret_data = {"value": idx_val}
+    return jsonify(ret_data)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
