e0fcb60fdbe51336473efab69196f9141fd78fd6
Tom Rafferty <thomasrafferty@gmail.com>
2016-09-22 15:20:57 +0000

    refactored to use setInterval with variable timing instead of recalling update function

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
-idx_val = 0
+idx = 0
 
 @app.before_request
 def before_request():
@@ -13,23 +13,18 @@ def before_request():
 def index():
     return render_template('index.html')
  
-@app.route('/echo/', methods=['GET'])
-def echo():
-    ret_data = {"value": request.args.get('echoValue')}
+@app.route('/get_next_idx/', methods=['GET'])
+def get_next_idx():
+    global idx
+    idx += 1
+    ret_data = {"value": idx}
     return jsonify(ret_data)
  
-@app.route('/idx/', methods=['GET'])
-def idx():
-    global idx_val
-    idx_val += 1
-    ret_data = {"value": idx_val}
-    return jsonify(ret_data)
- 
-@app.route('/reset/', methods=['GET'])
-def reset():
-    global idx_val
-    idx_val = 0
-    ret_data = {"value": idx_val}
+@app.route('/reset_idx/', methods=['GET'])
+def reset_idx():
+    global idx
+    idx = 0
+    ret_data = {"value": idx}
     return jsonify(ret_data)
 
 if __name__ == '__main__':
